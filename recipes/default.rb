ef4936a9d3fe1c218d1c67ed5599b6e5aec3b80e
xuyangbocn <xuyangbocn@users.noreply.github.com>
2020-10-28 13:13:23 +0800

    Update default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -17,16 +17,16 @@ Vault.configure do |config|
 	# The address of the Vault server, also read as ENV["VAULT_ADDR"]
 	config.address = "https://gcc.gov.sg:8200"
 	config.ssl_verify = false
-end 
-signature = `curl http://169.254.169.254/latest/dynamic/instance-identity/pkcs7`
-iam_role = `curl http://169.254.169.254/latest/meta-data/iam/security-credentials/`
-vault_token = Vault.auth.aws_ec2(iam_role, signature, '/v1/gcs-sm-ns-gvt-aws-auth/login')
-# vault_token = Vault.auth.aws_ec2(iam_role, signature, nil) #"s.C0mnpOzkC2hDz9NJgZWSIyXp"
+end
+
+iam_role = 'gvt-iam-auth-role'
+Vault.auth.aws_ec2(iam_role, signature, '/v1/gcs-sm-ns-gvt-aws-auth/login')
+# vault_token = Vault.auth.aws_ec2(iam_role, signature, nil) #"s.fs3SSbPSCmk7Q1yOqlN96Mee"
 
 hvault_read_vault 'Read secret kv-v2/my-secret' do
 	path "kv-v2/data/my-secret"
 	address 'https://gcc.gov.sg:8200'
-	token "#{vault_token}"
+	token Vault.token
 	notifies :create, "file[/tmp/test.txt]", :immediately
 end
 
