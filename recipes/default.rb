1c62ed19b259814efb2a0670ca79498709a87c4c
xuyangbocn <xuyangbocn@users.noreply.github.com>
2020-10-28 10:19:08 +0800

    Update default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -12,16 +12,16 @@
 #puts hv[:token]
 
 hvault_read_vault 'Read secret kv-v2/my-secret' do
-	path "gcs-sm-ns-gvt-aws-auth/kv-v2/my-secret"
+	path "kv-v2/data/my-secret"
 	address 'https://gcc.gov.sg:8200'
-	token 's.aePrn5xtnNr7H2FaTkkNwR0C'
+	token 's.C0mnpOzkC2hDz9NJgZWSIyXp'
 	notifies :create, "file[/tmp/test.txt]", :immediately
 end
 
 file '/tmp/test.txt' do
 	sensitive true
 	content lazy {
-	"chef password is:#{node.run_state["kv-v2/my-secret"].data[:data][:password]}"
+	"chef password is:#{node.run_state["kv-v2/data/my-secret"].data[:data][:password]}"
 	}
 	#action :nothing
 end
