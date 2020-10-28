665a5a97f03338c6c34d33b956365e95b629409f
xuyangbocn <xuyangbocn@users.noreply.github.com>
2020-10-28 10:42:06 +0800

    Update default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,3 +1,5 @@
+require 'vault'
+
 #
 # Cookbook:: hvault
 # Recipe:: default
@@ -11,17 +13,21 @@
 #hv = data_bag_item('secrets', 'hashi-vault')
 #puts hv[:token]
 
+signature = `curl http://169.254.169.254/latest/dynamic/instance-identity/pkcs7`
+iam_role = `curl http://169.254.169.254/latest/meta-data/iam/security-credentials/`
+vault_token = Vault.auth.aws_ec2(iam_role, signature, nil) #"s.C0mnpOzkC2hDz9NJgZWSIyXp"
+
 hvault_read_vault 'Read secret kv-v2/my-secret' do
 	path "kv-v2/data/my-secret"
 	address 'https://gcc.gov.sg:8200'
-	token 's.C0mnpOzkC2hDz9NJgZWSIyXp'
+	token "#{vault_token}"
 	notifies :create, "file[/tmp/test.txt]", :immediately
 end
 
 file '/tmp/test.txt' do
 	sensitive true
 	content lazy {
-	"chef password is:#{node.run_state["kv-v2/data/my-secret"].data[:data][:my-vault]}"
+	"chef password is:#{node.run_state["kv-v2/data/my-secret"].data[:data][:password]}"
 	}
 	#action :nothing
 end
